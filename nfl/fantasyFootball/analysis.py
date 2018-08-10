8a9feb198b715d789554f56cb51b7cb1e0ea391a
ethancarpenter <ethancarpenter00@comcast.net>
2018-08-10 12:35:19 -0400

    -

--- nfl/fantasyFootball/analysis.py
+++ nfl/fantasyFootball/analysis.py
@@ -1,32 +1,39 @@
 import pandas as pd
-from statistics import mean
+from statistics import mean, stdev
 import numpy as np
 
-positions = ('k', 'qb', 'rb', 'wr', 'te', 'def')
-numTeams = 12
-numStarters = {'k': 1, 
-               'qb': 1, 
+def analyze(numStarters, numTeams=12, how='mean'):
+    data = []
+    for pos in numStarters:
+        df = pd.read_csv('resources/{}.csv'.format(pos))
+        df.dropna(inplace=True)
+        if how == 'mean':
+            benchmark = mean(df['FPTS'][:numStarters[pos] * numTeams])
+            df['value'] = df['FPTS'] - benchmark
+        elif how == 'z':
+            dev = stdev(df['FPTS'][:numStarters[pos] * numTeams])
+            avg = mean(df['FPTS'][:numStarters[pos] * numTeams])
+            df['value'] = (df['FPTS'] - avg) / dev
+        
+        df['pos'] = pos
+        df = df[['Player', 'FPTS', 'value', 'pos']]
+        df.set_index(df['Player'])
+        
+        data.append(df)
+    return pd.concat(data)
+    
+numStarters = {'qb': 1, 
                'rb': 2, 
                'wr': 2, 
-               'te': 1, 
-               'def': 1}
-data = []
+               #'def': 1,
+               #'k': 1, 
+               #'te': 1
+               }
 
-for pos in positions:
-    df = pd.read_csv('resources/{}.csv'.format(pos))
-    df.dropna(inplace=True)
-    benchmark = mean(df['FPTS'][:numStarters[pos] * numTeams])
-    df['value'] = df['FPTS'] - benchmark
-    df['pos'] = pos
-    
-    df = df[['Player', 'FPTS', 'value', 'pos']]
-    df.set_index(df['Player'])
-    print(df.head())
-    
-    data.append(df)
-    
-merged = pd.concat(data)
-print(merged)
+merged = analyze(how='mean', numStarters=numStarters, numTeams=12)
 merged.sort_values('value', ascending=False, inplace=True)
-merged.reset_index()
+merged.reset_index(inplace=True, drop=True)
+merged.index += 1
 merged.to_csv('playerValues.csv')
+
+print(merged.head(25))
